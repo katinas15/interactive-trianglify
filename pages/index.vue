02aedcf775f1fe77989ec172c77e4f0b0ecc50d5
Laimonas Janutėnas <laimonas.janutenas@gmail.com>
2022-01-30 17:48:04 +0200

    Background: refactored to another file

--- pages/index.vue
+++ pages/index.vue
@@ -8,18 +8,7 @@
 </template>
 
 <script>
-import trianglify from 'trianglify'
-import {
-  generatePoints,
-  generateMovementDirection,
-  generateCircleSize,
-  MAX_BACKGROUND_CIRCLE_RADIUS,
-} from '../utils/trianglifyBackground'
-
-const FRAME_DELAY = 1000 / 30 // 30fps
-const CLICK_TEXT_OFFSET = 10
-const CLICK_TEXT_FONT = '14px Trebuchet MS'
-
+import Background from '../utils/trianglifyBackground.ts'
 export default {
   data() {
     return {}
@@ -28,119 +17,13 @@ export default {
     const width = window.innerWidth
     const height = window.innerHeight
     const canvas = document.querySelector('#bg')
-    const ctx = canvas.getContext('2d')
-    const circleColor = '#000'
-
-    let points = generatePoints({
+    const background = new Background({
       width,
       height,
-      cellSize: 100,
+      canvas,
+      cellSize: 110,
       variance: 0,
     })
-
-    const movementEveryFrame = points.map(() => {
-      return generateMovementDirection()
-    })
-
-    const circleSizes = points.map(() => {
-      return generateCircleSize()
-    })
-
-    let enableCallMouse = true
-    let clicked = false
-    let mousePos = [0, 0]
-
-    document.addEventListener('click', (event) => {
-      points.push([event.clientX, event.clientY])
-      movementEveryFrame.push(generateMovementDirection())
-      circleSizes.push(generateCircleSize())
-      clicked = true
-    })
-
-    document.addEventListener('mousemove', (event) => {
-      if (!enableCallMouse) return
-      enableCallMouse = false
-      mousePos = [event.clientX, event.clientY]
-      setTimeout(() => {
-        enableCallMouse = true
-      }, FRAME_DELAY)
-    })
-
-    setInterval(function () {
-      points = points.map(([x, y], index) => {
-        if (x > width || x < 0) {
-          movementEveryFrame[index][0] *= -1
-        }
-
-        if (y > height || y < 0) {
-          movementEveryFrame[index][1] *= -1
-        }
-        const [xSpeed, ySpeed] = movementEveryFrame[index]
-        const [xMove, yMove] = [x + xSpeed, y + ySpeed]
-        return [xMove, yMove]
-      })
-
-      const localPoints = [...points]
-      if (mousePos) {
-        localPoints.push([mousePos[0], mousePos[1]])
-      }
-
-      const pattern = trianglify({
-        width,
-        height,
-        // xColors: ['#000000', '#0C0C0C', '#000000'],
-        xColors: [
-          '#000000',
-          '#0D0D0D',
-          '#351409',
-          '#a15501',
-          '#d0902f',
-          '#fff69f',
-          '#fdd870',
-          '#d0902f',
-          '#a15501',
-          '#351409',
-          '#0D0D0D',
-          '#000000',
-        ],
-
-        // xColors: ['#fff69f', '#fdd870', '#d0902f', '#a15501', '#351409'],
-        // xColors: ['#C78B06', '#C78B06', '#5D430A', '#DED765', '#310D01'],
-        points: localPoints,
-      })
-
-      pattern.toCanvas(canvas)
-
-      const drawCircle = (
-        ctx,
-        point,
-        radius = MAX_BACKGROUND_CIRCLE_RADIUS,
-        color
-      ) => {
-        ctx.beginPath()
-        ctx.arc(point[0], point[1], radius, 0, 2 * Math.PI)
-
-        ctx.closePath()
-
-        if (color) {
-          ctx.fillStyle = color
-          ctx.fill()
-        }
-      }
-
-      pattern.points.forEach((point, index) => {
-        drawCircle(ctx, point, circleSizes[index], circleColor)
-      })
-
-      if (!clicked) {
-        ctx.font = CLICK_TEXT_FONT
-        ctx.fillText(
-          'Click!',
-          mousePos[0] + CLICK_TEXT_OFFSET,
-          mousePos[1] - CLICK_TEXT_OFFSET
-        )
-      }
-    }, FRAME_DELAY)
   },
 }
 </script>
---
+++ utils/trianglifyBackground.ts
@@ -0,0 +1,191 @@
+import trianglify from 'trianglify'
+
+const BACKGROUND_MOVEMENT_SPEED = 0.5
+const MIN_BACKGROUND_CIRCLE_RADIUS = 2
+const FRAME_DELAY: number = 1000 / 30 // 30fps
+const MAX_BACKGROUND_CIRCLE_RADIUS = 5
+const CLICK_TEXT_OFFSET = 10
+const CLICK_TEXT_FONT = '14px Trebuchet MS'
+
+interface Options {
+  canvas: HTMLCanvasElement
+  width: number
+  height: number
+  cellSize: number
+  variance: number
+}
+
+const generatePoints = (opts: Options) => {
+  const { width, height, cellSize, variance } = opts
+
+  // pad by 2 cells outside the visible area on each side to ensure we fully
+  // cover the 'artboard'
+  const colCount = Math.floor(width / cellSize) + 4
+  const rowCount = Math.floor(height / cellSize) + 4
+
+  // determine bleed values to ensure that the grid is centered within the
+  // artboard
+  const bleedX = (colCount * cellSize - width) / 2
+  const bleedY = (rowCount * cellSize - height) / 2
+
+  // apply variance to cellSize to get cellJitter in pixels
+  const cellJitter = cellSize * variance
+  const getJitter = () => (Math.random() - 0.5) * cellJitter
+
+  const pointCount = colCount * rowCount
+
+  const halfCell = cellSize / 2
+
+  const points = Array(pointCount)
+    .fill(null)
+    .map((_, i) => {
+      const col = i % colCount
+      const row = Math.floor(i / colCount)
+
+      // [x, y, z]
+      return [
+        -bleedX + col * cellSize + halfCell + getJitter(),
+        -bleedY + row * cellSize + halfCell + getJitter(),
+      ]
+    })
+
+  return points
+}
+
+const generateMovementDirection = () => {
+  const x = (Math.random() * 2 - 1) * BACKGROUND_MOVEMENT_SPEED
+  const y = (Math.random() * 2 - 1) * BACKGROUND_MOVEMENT_SPEED
+  return [x, y]
+}
+
+const generateCircleSize = () => {
+  const max = MAX_BACKGROUND_CIRCLE_RADIUS
+  const min = MIN_BACKGROUND_CIRCLE_RADIUS
+  return Math.random() * (max - min) + min
+}
+
+export default class Background {
+  private ctx: CanvasRenderingContext2D
+  private points: number[][] = []
+  private movementDirections: number[][] = []
+  private circleSizes: number[] = []
+  private clicked = false
+  private enableCallMouse = true
+  private mousePos = [0, 0]
+
+  constructor(public opts: Options) {
+    this.points = generatePoints(opts)
+    this.movementDirections = this.points.map(() => {
+      return generateMovementDirection()
+    })
+    this.circleSizes = this.points.map(() => {
+      return generateCircleSize()
+    })
+
+    this.ctx = this.opts.canvas.getContext('2d')
+
+    this.handleClicks()
+    this.handleMouseMovement()
+
+    this.updateFrame.bind(this)
+    setInterval(this.updateFrame, FRAME_DELAY)
+  }
+
+  private handleClicks = () => {
+    this.opts.canvas.addEventListener('click', (event) => {
+      this.points.push([event.clientX, event.clientY])
+      this.movementDirections.push(generateMovementDirection())
+      this.circleSizes.push(generateCircleSize())
+      this.clicked = true
+    })
+  }
+
+  private handleMouseMovement = () => {
+    this.opts.canvas.addEventListener('mousemove', (event) => {
+      if (!this.enableCallMouse) return
+      this.enableCallMouse = false
+      this.mousePos = [event.clientX, event.clientY]
+      setTimeout(() => {
+        this.enableCallMouse = true
+      }, FRAME_DELAY)
+    })
+  }
+
+  private updateFrame = () => {
+    this.points = this.points.map(([x, y], index) => {
+      if (x > this.opts.width || x < 0) {
+        this.movementDirections[index][0] *= -1
+      }
+
+      if (y > this.opts.height || y < 0) {
+        this.movementDirections[index][1] *= -1
+      }
+      const [xSpeed, ySpeed] = this.movementDirections[index]
+      const [xMove, yMove] = [x + xSpeed, y + ySpeed]
+      return [xMove, yMove]
+    })
+
+    const localPoints = [...this.points]
+    if (this.mousePos) {
+      localPoints.push([this.mousePos[0], this.mousePos[1]])
+    }
+
+    const pattern = trianglify({
+      width: this.opts.width,
+      height: this.opts.height,
+      // xColors: ['#000000', '#0C0C0C', '#000000'],
+      xColors: [
+        '#000000',
+        '#0D0D0D',
+        '#351409',
+        '#a15501',
+        '#d0902f',
+        '#fff69f',
+        '#fdd870',
+        '#d0902f',
+        '#a15501',
+        '#351409',
+        '#0D0D0D',
+        '#000000',
+      ],
+
+      // xColors: ['#fff69f', '#fdd870', '#d0902f', '#a15501', '#351409'],
+      // xColors: ['#C78B06', '#C78B06', '#5D430A', '#DED765', '#310D01'],
+      points: localPoints,
+    })
+
+    pattern.toCanvas(this.opts.canvas)
+
+    const drawCircle = (
+      ctx: CanvasRenderingContext2D,
+      point: number[],
+      radius = MAX_BACKGROUND_CIRCLE_RADIUS,
+      color: string
+    ) => {
+      ctx.beginPath()
+      ctx.arc(point[0], point[1], radius, 0, 2 * Math.PI)
+
+      ctx.closePath()
+
+      if (color) {
+        ctx.fillStyle = color
+        ctx.fill()
+      }
+    }
+    const circleColor = '#000'
+
+    if (!this.ctx) return
+    this.points.forEach((point: number[], index: number) => {
+      drawCircle(this.ctx, point, this.circleSizes[index], circleColor)
+    })
+
+    if (!this.clicked && this.ctx) {
+      this.ctx.font = CLICK_TEXT_FONT
+      this.ctx.fillText(
+        'Click!',
+        this.mousePos[0] + CLICK_TEXT_OFFSET,
+        this.mousePos[1] - CLICK_TEXT_OFFSET
+      )
+    }
+  }
+}
